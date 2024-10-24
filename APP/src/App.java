16f3352d2d2d9074eb78ccdee82ab3cda077f345
Eden <ikdui@plaiaundi.net>
2024-10-24 10:51:14 +0200

    commit nuevoMetodo

--- APP/src/App.java
+++ APP/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+    public void metodo1(){
+
+        System.out.println("Hola he añadido une metodo nuevo")
     }
 }
