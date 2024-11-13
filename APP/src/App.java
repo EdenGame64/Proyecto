e1c370fc5ed302499edd602827b910af147bf56b
Anartz Arri <ikdgx@plaiaundi.net>
2024-11-13 11:48:53 +0100

    commit a ver que pasa

--- APP/src/App.java
+++ APP/src/App.java
@@ -5,6 +5,7 @@ public class App {
     }
     public void metodo1(){
 
-        System.out.println("Hola he añadido une metodo nuevo")
+        System.out.println("Hola he añadido une metodo nuevo");
+        System.out.println("Buenas tardes");
     }
 }
